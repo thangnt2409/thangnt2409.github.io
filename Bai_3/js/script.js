ebae8cbe74dd9e865467120a201a79540e3315b2
thangnt2409 <thangntit24@gmail.com>
2016-10-06 18:02:40 +0700

    de3

---
+++ Bai_3/js/script.js
@@ -0,0 +1,34 @@
+var a= true;
+function one() {
+	if(a==false){
+				document.getElementById('nd1').style.height="95px";
+				a=true;
+				}else
+				{
+				document.getElementById('nd1').style.height="0px";
+			a=false;
+				}
+
+}
+function two() {
+	
+	if(a==false){
+				document.getElementById('nd2').style.height="95px";
+				a=true;
+				}else
+				{
+				document.getElementById('nd2').style.height="0px";
+				a=false;
+				}
+}
+function three() {
+	
+	if(a==false){
+				document.getElementById('nd3').style.height="95px";
+				a=true;
+				}else
+				{
+				document.getElementById('nd3').style.height="0px";
+				a=false;
+				}
+}
